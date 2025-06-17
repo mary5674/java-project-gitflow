fa56bbf75efc690e18ed6ada2a12d9262fcd2bc1
mary5674 <94326118+mary5674@users.noreply.github.com>
2025-06-17 02:32:29 +0000

    Saludo corregido

--- Saludo.java
+++ Saludo.java
@@ -1,5 +1,9 @@
 public class Saludo{
     public String generaSaludo(String nombre){
+        if(nombre == null || nombre.isEmpty()){
+            return "Hola, invitado!"
+        }
+        
         return "Hola, " + nombre + "!";
     }
 }
